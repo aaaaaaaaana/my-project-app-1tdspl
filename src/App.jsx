bcd3bfc65d48866d72c75484316a7ba04e250da6
aaaaaaaaana <rm551401@fiap.com.br>
2023-08-22 10:19:22 -0300

    DIVS

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Cabecalho 
+
 import React from "react";
 
 
@@ -5,19 +7,38 @@ export default function App() {
   return (
 
     <>
+    <div className="container">
+
+    <Cabecalho/>
+
+    <header>
+      <h1>First deploy in to vercel</h1>
+    </header>
+
+    </div>
     
+
+    <section>
     <h1>First deploy in to vercel</h1>
     <h2>vite + react</h2>
     <p>coded by - rm551401</p>
 
     <div>
+
       <p>Turma - 1tdspl</p>
       <p>Fiap</p>
 
-
     </div>
+    </section>
+    
+    <footer>
+      <a href="#">github</a>
+      <a href="#">likedin</a>
+      <a href="#">twitter</a>
+    </footer>
 
     </>
+    
 
   )
 }
