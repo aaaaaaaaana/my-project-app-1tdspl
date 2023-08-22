9b3a7f6a18bc586a1bb7f7367b3f91df4885612a
aaaaaaaaana <rm551401@fiap.com.br>
2023-08-22 09:24:10 -0300

    inicializando tudo de verdade

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export default function App() {
+  return (
+
+    <>
+    
+    <h1>First deploy in to vercel</h1>
+    <h2>vite + react</h2>
+    <p>coded by - rm551401</p>
+
+    </>
+
+  )
+}
+
